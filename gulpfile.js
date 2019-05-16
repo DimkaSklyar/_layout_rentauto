43aa51e2ab054adfd18b8db79b22c3b7a2805ad0
Dimka <dimkasklyar@mail.ru>
2019-05-16 16:40:57 +0300

    v0.2

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,10 @@ gulp.task('sass', function(){
 gulp.task('js', function() {
 	return gulp.src([
 			'src/libs/jquery/dist/jquery.min.js',
+			'src/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
 			'src/libs/parallax.min.js',
+			'src/libs/mixitup-v3/dist/mixitup.min.js',
+			'src/libs/jquery.mixitup.pagination.min.js',
 			'src/js/common.js', // Always at the end
 			])
 		.pipe(concat('scripts.min.js'))
--- src/js/common.js
+++ src/js/common.js
@@ -1,11 +1,28 @@
-(function($) {
-  $(function() {
-  
-    
-  
-  });
-  })(jQuery);
 
 $(document).ready(function () {
+
+  var mixer = mixitup(".container-mix", {
+    pagination: {
+      limit: 6 // impose a limit of 8 targets per page
+    }
+  });
+
   $('input, select').styler();
+
+
+  $('.popup-with-zoom-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
 });
